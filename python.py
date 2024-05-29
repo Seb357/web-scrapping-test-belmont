e37041232566d8dca625fd360dfa91972c5001e7
Senzu357 <sebi30320@gmail.com>
2024-05-29 14:28:21 +0200

    modification du script pour récupérer les 5 premières pages du site

--- python.py
+++ python.py
@@ -2,3 +2,26 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+base_url = 'http://quotes.toscrape.com/page/{}/'
+
+def get_quotes_from_page(page_number):
+    url = base_url.format(page_number)
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    quotes = []
+    
+    for quote in soup.find_all('div', class_='quote'):
+        text = quote.find('span', class_='text').get_text()
+        author = quote.find('small', class_='author').get_text()
+        tags = [tag.get_text() for tag in quote.find_all('a', class_='tag')]
+        quotes.append({
+            'texte': text,
+            'auteur': author,
+            'tags': tags
+        })
+    
+    return quotes
+
+all_quotes = []
+for i in range(1, 6):
+    all_quotes.extend(get_quotes_from_page(i))
